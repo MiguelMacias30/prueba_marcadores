6e53479fb5ff173ee96831b7acb5b80a1ad7a389
Miguel Macias <miguelfernandomacias@gmail.com>
2020-11-06 13:53:31 -0400

    ADD:routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'api/index'
   resources :markers
   resources :subcategories
   resources :categories
   resources :types
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'api/category', to: 'api#index'
+  root :'markers#index'
 end
