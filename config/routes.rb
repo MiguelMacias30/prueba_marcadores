6c202342b3ad2ca8ec37f364c6289c5b58167af7
Miguel Macias <miguelfernandomacias@gmail.com>
2020-11-06 13:57:32 -0400

    ADD:layout links

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :types
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'api/category', to: 'api#index'
-  root :'markers#index'
+  root 'markers#index'
 end
