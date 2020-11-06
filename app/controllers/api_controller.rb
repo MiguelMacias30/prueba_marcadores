7b63f916954fccd3f49249a39db9a8867e856df6
Miguel Macias <miguelfernandomacias@gmail.com>
2020-11-06 14:26:05 -0400

    ADD:api method

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,19 @@
 class ApiController < ApplicationController
   def index
+    @categories = Category.all
+    @all = []
+
+    @categories.each do |category|
+        hash = {
+          name: category.name,
+          subcategories: category.subcategories,
+          markers: Marker.where(subcategory_id: category.subcategories.map{|cat| cat.id})
+
+        }
+      @all << hash
+    end
+
+
+    render json: @all
   end
 end
