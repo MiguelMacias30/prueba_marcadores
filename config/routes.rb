43cfe515db79565e8cb67affa3f7ce052f6d0a6a
Miguel Macias <miguelfernandomacias@gmail.com>
2020-11-06 13:02:57 -0400

    ADD:scaffold and migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :markers
+  resources :subcategories
+  resources :categories
+  resources :types
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
